e1f1bb38c4898a4568fc7580f90431989177570d
Marcos Salomão <salomao.marcos@gmail.com>
2023-09-27 02:54:59 -0300

    #4 add english tutorial

--- web/src/features/Tutorials/FixedIncomeTutorial.tsx
+++ web/src/features/Tutorials/FixedIncomeTutorial.tsx
@@ -68,6 +68,68 @@ const pt = (
         <p><b>Diversificação:</b> Para quem já possui outros investimentos e deseja diversificar, os pós-fixados oferecem uma alternativa que varia de acordo com o comportamento do mercado.</p>
     </div>)
 
+const en = (
+    <div className="tutorial-container">
+    <h1>What is Fixed Income Investment?</h1>
+    <p>
+    Imagine you lent money to a friend, and they promised to return the same amount plus a little extra as a thank you. 
+    </p>
+    <p>
+    Fixed income works in a similar way, but instead of a friend, it's a financial institution. When investing, 
+    you "lend" money to it, and in return, you receive your money back plus the agreed interest.
+    </p>
+    <h2>
+    Types of Fixed Income: CDB and LCA/LCI
+    </h2>
+    <h3>
+    CDB (Bank Deposit Certificate)
+    </h3>
+    <p>
+    It's like a "loan" you make to the bank. In exchange, they promise to return the money with interest after a determined period.
+    </p>
+    <h3>
+    LCA (Agribusiness Credit Letter) and LCI (Real Estate Credit Letter)
+    </h3>
+    <p>
+        Similar to CDB, but the invested money goes towards financing in the agribusiness sector (LCA) or real estate (LCI). 
+        One benefit is that they are <u>exempt from Income Tax</u> for individuals.
+    </p>
+    <h3>Benefits of Fixed Income</h3>
+    <p><b>Security:</b> Fixed income is considered one of the safest investments.</p>
+    <p><b>Predictability:</b> You know how much you will receive at the end of the period.</p>
+    <p><b>Diversity:</b> There are several options to choose from according to your profile.</p>
+    <h3>Risks</h3>
+    <p><b>Liquidity:</b> Depending on the investment, it may not be easy to withdraw the money before the term.</p>
+    <p><b>Profitability:</b> In some cases, it may not exceed inflation, causing you to lose purchasing power.</p>
+    <h2>Categories: Pre-fixed and Post-fixed</h2>
+    <h3>Pre-fixed</h3>
+    <p>
+        In this type, the return is defined at the time you invest. For example, 
+        if they say it will yield 10% per year, that will be your return, regardless of what happens in the economy.
+    </p>
+    <h3>Post-fixed</h3>
+    <p>
+    The return varies according to an index. The most common are:
+    </p>
+    <p><b>CDI:</b> Acronym for Interbank Deposit Certificate. If an investment yields 100% of CDI and this index was 10% per year, your money will yield those 10%.</p>
+    <p><b>Inflation (CPI):</b> The profitability is tied to inflation. For example, if the investment is CPI + 3% per year and inflation was 5%, your return will be 8%.</p>
+    <h2>When to choose Pre-fixed or Post-fixed investments?</h2>
+    <p>
+        The choice between investing in pre-fixed or post-fixed fixed income is not an exact science, 
+        but there are some situations and contexts that can help guide your decision.
+    </p>
+    <h3>Pre-fixed</h3>
+    <p><b>Return Predictability:</b> If you like the idea of knowing exactly how much your money will yield at the end of the investment, pre-fixed is the best option.</p>
+    <p><b>Expectation of falling interest rates:</b> If you believe that the economy's interest rates will fall in the future, it might be interesting to "lock in" a yield rate now. For example, if the offered yield is 10% per year and you expect interest rates to drop to 5%, you ensure a higher profitability than what would be offered in the future.</p>
+    <p><b>Financial planning:</b> For specific financial goals and short/medium term, pre-fixed allows you to know the exact value you will have at the end of the period.</p>
+    <h3>Post-fixed</h3>
+    <p><b>Protection against inflation:</b> Investments tied to the CPI ensure that your money yields above inflation, preserving your purchasing power. Ideal for long-term goals, such as retirement.</p>
+    <p><b>Expectation of rising interest rates:</b> If there's an expectation that the economy's interest rates will rise, investing in post-fixed tied to CDI, for example, might yield better returns. This is because the CDI tends to follow the basic interest rate of the economy.</p>
+    <p><b>Flexibility:</b> Since the yield of post-fixed is tied to economic indices, it might be interesting for those who want to follow the market and have the possibility of variable gains.</p>
+    <p><b>Diversification:</b> For those who already have other investments and want to diversify, post-fixed offers an alternative that varies according to market behavior.</p>
+</div>)
+
 const translations: { [key: string]: any } = {
-    pt
+    pt,
+    en
   };
