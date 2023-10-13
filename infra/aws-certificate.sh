81f0f6ddc4129aba3669729db485d2c96f525e06
Marcos Salomão <salomao.marcos@gmail.com>
2023-10-13 11:08:48 -0300

    Release new version

--- infra/aws-certificate.sh
+++ infra/aws-certificate.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <Domain Name>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <Component> <Environemnt> <Domain Name>"
     exit 1
 fi
 
-DOMAIN_NAME=$1
+COMPONENT=$1
+ENVIRONMENT=$2
+DOMAIN_NAME=$3
 
-CFN_FILE="aws-certificate.yml"
-STACK_NAME="invistus-certificate"
 APPLICATION_NAME="invistus"
+CFN_FILE="aws-certificate.yml"
+STACK_NAME="invistus-certificate-$COMPONENT-$ENVIRONMENT"
 COMPONENT="certificate"
 # To use a certificate in AWS Certificate Manager (ACM) to require HTTPS between viewers and CloudFront, 
 # make sure you request (or import) the certificate in the US East (N. Virginia) Region (us-east-1).
@@ -18,6 +20,7 @@ AWS_REGION="us-east-1"
 
 PARAMS=(
   DomainName="${DOMAIN_NAME}"
+  Environment="${ENVIRONMENT}"
 )
 
 echo "Creating certficate stack '$STACK_NAME' based on file '$CFN_FILE'"
@@ -27,7 +30,7 @@ aws cloudformation deploy \
   --stack-name "$STACK_NAME" \
   --parameter-overrides "${PARAMS[@]}" \
   --region "$AWS_REGION" \
-  --tags Service="${APPLICATION_NAME}" Component="${COMPONENT}"
+  --tags Service="${APPLICATION_NAME}" Component="${COMPONENT}" Environment="${ENVIRONMENT}"
 
 DEPLOY_EXIT_CODE=$?
 
