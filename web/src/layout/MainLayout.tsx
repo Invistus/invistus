047191b36e88abd5af583bb4bb3a75e0855e3003
Marcos Salomão <salomao.marcos@gmail.com>
2023-10-14 14:34:32 -0300

    #35 Implement the basics of responsive design (#36)
    * #35 Implement the basics of responsive design
    * #35 Adjust responsive styles and animations

--- web/src/layout/MainLayout.tsx
+++ web/src/layout/MainLayout.tsx
@@ -1,16 +1,25 @@
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren, useState } from 'react';
 import Header from './Header';
 import LeftMenu from './LeftMenu';
 import './layout.css';
 
 
 const MainLayout = (props : PropsWithChildren) => {
+    
+    const [ menuVisible, setMenuVisible ] = useState(false);
+
+    const toggleMenu = () => {
+        const visible = !menuVisible;
+        setMenuVisible(visible);
+        return visible;
+    };
+
     return (
         <div className="layout-container">
-            <Header />
+            <Header toggleMenu={toggleMenu} activedMenu={menuVisible}/>
             <div className="layout-body">
-                <LeftMenu />
-                <div className="content">
+                <LeftMenu menuVisible={menuVisible} toggleMenu={toggleMenu}/>
+                <div className={`content ${menuVisible && "menuVisible"}`}>
                     {props.children}
                 </div>
             </div>
