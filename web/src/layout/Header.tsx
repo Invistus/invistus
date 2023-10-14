ff22107332d884418dab4b2f32e27253143c8c87
Marcos Salomão <salomao.marcos@gmail.com>
2023-10-14 10:39:19 -0300

    #35 Adjust responsive styles and animations

--- web/src/layout/Header.tsx
+++ web/src/layout/Header.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 interface HeaderProps {
-    toggleMenu: () => void
+    toggleMenu: () => boolean;
+    activedMenu: boolean;
   };
 
-const Header: React.FC<HeaderProps> = ({ toggleMenu }) => {
+const Header: React.FC<HeaderProps> = ({ toggleMenu, activedMenu }) => {
+
+    const [ key, setKey ] = useState(0);
+
+    const clickMenuButton = () => {
+        setKey(prevKey => prevKey + 1);
+        toggleMenu();
+    }
+
     return (
         <header className="text-white p-2 d-flex justify-content-between">
             <div className="logo">
@@ -13,8 +22,12 @@ const Header: React.FC<HeaderProps> = ({ toggleMenu }) => {
                     <img src="/images/logo/invistus_logo.svg" alt="Logo" />
                 </Link>
             </div>
-            <button className="btn menu-button" onClick={toggleMenu}>
-                <i className="fa fa-solid fa-bars fa-lg"></i>
+            <button className="btn menu-button active" onClick={clickMenuButton}>
+                <span>
+                    { activedMenu 
+                        ? <i key={key} className="fa fa-solid fa-xmark fa-2x"></i>
+                        : <i key={key} className="fa fa-solid fa-bars fa-2x"></i> }
+                </span>
             </button>
             {/* <button className="btn btn-light">UserProfile</button> */}
         </header>
--- web/src/layout/MainLayout.tsx
+++ web/src/layout/MainLayout.tsx
@@ -9,15 +9,17 @@ const MainLayout = (props : PropsWithChildren) => {
     const [menuVisible, setMenuVisible] = useState(false);
 
     const toggleMenu = () => {
-      setMenuVisible(!menuVisible);
+        const visible = !menuVisible;
+        setMenuVisible(visible);
+        return visible;
     };
 
     return (
         <div className="layout-container">
-            <Header toggleMenu={toggleMenu} />
+            <Header toggleMenu={toggleMenu} activedMenu={menuVisible}/>
             <div className="layout-body">
                 <LeftMenu menuVisible={menuVisible} toggleMenu={toggleMenu}/>
-                <div className="content">
+                <div className={`content ${menuVisible && "menuVisible"}`}>
                     {props.children}
                 </div>
             </div>
